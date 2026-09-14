83c1601aa5b74e22ec280592d3f7b47206999163
Ajmal Razaq <theajmalrazaq@gmail.com>
2026-09-14 18:53:41 +0500

    Rig 4 drone propellers with individual motor hub pivots and Cesium NodeTransformationProperty dynamic rotation

--- frontend/js/drone3d.js
+++ frontend/js/drone3d.js
@@ -654,31 +654,21 @@ class Drone3DVisualizer {
         const initialQuat = Cesium.Transforms.headingPitchRollQuaternion(initialPos, hpr);
 
         // High-performance dynamic node transformations for spinning propellers at motor RPM
+        const createPropNodeTransform = (hubX, hubY, hubZ, propIdx) => {
+            return new Cesium.NodeTransformationProperty({
+                translation: new Cesium.ConstantProperty(new Cesium.Cartesian3(hubX, hubY, hubZ)),
+                rotation: new Cesium.CallbackProperty((time, result) => {
+                    return Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_Y, this.propAngles[propIdx], result || new Cesium.Quaternion());
+                }, false),
+                scale: new Cesium.ConstantProperty(new Cesium.Cartesian3(1.0, 1.0, 1.0))
+            });
+        };
+
         const nodeTransformations = {
-            "prop_front_right": new Cesium.TranslationRotationScale({
-                translation: new Cesium.Cartesian3(2.84905, 0.45733, -1.61349),
-                rotation: new Cesium.CallbackProperty((time, result) => {
-                    return Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_Y, this.propAngles[0], result || new Cesium.Quaternion());
-                }, false)
-            }),
-            "prop_front_left": new Cesium.TranslationRotationScale({
-                translation: new Cesium.Cartesian3(-2.77596, 0.46271, -1.57663),
-                rotation: new Cesium.CallbackProperty((time, result) => {
-                    return Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_Y, this.propAngles[1], result || new Cesium.Quaternion());
-                }, false)
-            }),
-            "prop_rear_left": new Cesium.TranslationRotationScale({
-                translation: new Cesium.Cartesian3(-2.46222, 0.45717, 2.45253),
-                rotation: new Cesium.CallbackProperty((time, result) => {
-                    return Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_Y, this.propAngles[2], result || new Cesium.Quaternion());
-                }, false)
-            }),
-            "prop_rear_right": new Cesium.TranslationRotationScale({
-                translation: new Cesium.Cartesian3(2.44971, 0.48172, 2.58754),
-                rotation: new Cesium.CallbackProperty((time, result) => {
-                    return Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_Y, this.propAngles[3], result || new Cesium.Quaternion());
-                }, false)
-            })
+            "prop_front_right": createPropNodeTransform(2.84905, 0.45733, -1.61349, 0),
+            "prop_front_left": createPropNodeTransform(-2.77596, 0.46271, -1.57663, 1),
+            "prop_rear_left": createPropNodeTransform(-2.46222, 0.45717, 2.45253, 2),
+            "prop_rear_right": createPropNodeTransform(2.44971, 0.48172, 2.58754, 3)
         };
 
         this.droneEntity = this.viewer.entities.add({
